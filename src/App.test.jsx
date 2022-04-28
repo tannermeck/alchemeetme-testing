706923427e18fb20019f32529d910d9b0333ec49
TANNER MECK <steve.meck246@gmail.com>
2022-04-27 18:40:12 -0700

    tests passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,36 +2,15 @@
 // 🚨🚨 https://mswjs.io/docs/ 🚨🚨
 
 import { screen, render } from '@testing-library/react'
-// 🚨
-// import rest
-// import setupServer
+import { rest } from 'msw'
+import { server } from './setupTests'
 import App from './App'
-
-const user = {
-  id: 1,
-  created_at: '2021-12-13T00:17:29+00:00',
-  // 🚨 Add a name here
-  name: '',
-  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-  motto: 'Res Non Verba',
-  color: 'crimson',
-}
-
-// 🚨 Create your server
-
-// 🚨 Listen for server start
-beforeAll()
-
-// 🚨 Close server when complete
-afterAll()
 
 test('Should render the header', async () => {
   render(<App />)
   const banner = screen.getByRole('banner')
   const headerImg = screen.getByAltText(/alchemy/i)
-  const profileName = await screen.findByText(user.name)
+  const profileName = await screen.findByText('Tanman')
 
   expect(banner).toHaveStyle({
     background: 'var(--grey)',
@@ -51,8 +30,14 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
     motto: 'Res Non Verba',
     color: 'crimson',
   }
+  server.use(
+    rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`, (req, res, ctx) => {
+      return res(ctx.json([sasuke]))
+    })
+  )
 
   // 🚨 Use the server to change the response for this test
+  // const server = server.use()
 
   render(<App />)
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,31 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+const user = {
+  id: 1,
+  created_at: '2021-12-13T00:17:29+00:00',
+  name: 'Tanman',
+  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+  motto: 'Res Non Verba',
+  color: 'crimson',
+}
+
+// 🚨 Create your server
+// eslint-disable-next-line import/prefer-default-export
+export const server = setupServer(
+  rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`, (req, res, ctx) => {
+    return res(ctx.json([user]))
+  })
+)
+
+// 🚨 Listen for server start
+beforeAll(() => server.listen())
+
+// 🚨 Close server when complete
+afterAll(() => server.close())
